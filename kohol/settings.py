3f181bf245bb59ad03ce30f76e99c2868a899d9b
Iornav <38967587+Iornav@users.noreply.github.com>
2022-06-09 16:24:38 +0000

    Update settings.py
    Security Key

--- kohol/settings.py
+++ kohol/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s089qw7m@bbe#c0+sz0di=d2)*jyg_j&7^dcq^&)+9j4v7t(3v'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
