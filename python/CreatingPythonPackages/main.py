858e3c07545d81f8c13197149535905161ff8f2e
Iornav <38967587+Iornav@users.noreply.github.com>
2022-06-13 19:04:02 +0000

    Create main.py

---
+++ python/CreatingPythonPackages/main.py
@@ -0,0 +1,31 @@
+import random
+
+
+def rock_paper_scissors():
+    option_list = ['R', 'P', 'S']
+    player_choice = str(input('Choose one: R, P, S: \n'))
+    player_choice = player_choice.upper()
+    option = random.choice(option_list)
+    if player_choice in option_list:
+
+        print('Computer chooses:', option)
+        if player_choice == option:
+            print('Tie!')
+        elif player_choice == 'R' and option == 'S':
+            print('You win!')
+        elif player_choice == 'R' and option == 'P':
+            print('You lose!')
+        elif player_choice == 'P' and option == 'R':
+            print('You win!')
+        elif player_choice == 'P' and option == 'S':
+            print('You lose!')
+        elif player_choice == 'S' and option == 'P':
+            print('You win!')
+        elif player_choice == 'S' and option == 'R':
+            print('You lose!')
+    elif player_choice != 'R' or 'P' or 'S':
+        print('Invalid input!')
+        rock_paper_scissors()
+
+
+rock_paper_scissors()
